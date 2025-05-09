4d202ac6922049f2dc54d10a96d2e92837c9023f
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-09 14:21:56 +0200

    secondary button statisk

--- src/components/ButtonSecondary.jsx
+++ src/components/ButtonSecondary.jsx
@@ -1,5 +1,9 @@
 const ButtonSecondary = () => {
-  return <div></div>;
+  return (
+    <button className="border-3 border-lime-400 px-8 cursor-pointer">
+      <h4 className="font-semibold text-lime-400">Køb billetter</h4>
+    </button>
+  );
 };
 
 export default ButtonSecondary;
