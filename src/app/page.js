b0d2f28651e00eaf1788cfac59bdb95865a0a6c4
Anarikke <anla0008@stud.kea.dk>
2025-05-09 12:39:15 +0200

    Global.css

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="col-start-2">
       <h1>Dav</h1>
       <h2>Dav</h2>
-      <h3>Dav</h3>
+      <h3 className="mt-space-m">Dav</h3>
       <h4>Dav</h4>
       <h5>Dav</h5>
       <p>Dav</p>
