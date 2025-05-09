c559a1e81cee7e21acc315c411b763cae8c00a8a
Anarikke <anla0008@stud.kea.dk>
2025-05-09 11:27:00 +0200

    Opsætning

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="col-start-2">
+      <h1>Dav</h1>
+      <h2>Dav</h2>
+      <h3>Dav</h3>
+      <h4>Dav</h4>
+      <h5>Dav</h5>
+      <p>Dav</p>
+    </div>
+  );
 }
