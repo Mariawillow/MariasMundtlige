441ac489c105ece64aba61cd12d98f6498a68ea7
Anarikke <anla0008@stud.kea.dk>
2025-05-12 09:35:44 +0200

    Shadcn - dropdown

--- src/components/(listeView)/DropdownLocation.jsx
+++ src/components/(listeView)/DropdownLocation.jsx
@@ -1,25 +1,48 @@
+"use client";
+
 import {
   DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { IoIosArrowDown } from "react-icons/io";
+import React, { useState } from "react";
 
-const DropDownLocation = () => {
-  return <div>
-    <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+export default function LocationDropdown() {
+  // Her holder vi styr på, om dropdownen er åben eller lukket
+  const [open, setOpen] = useState(false);
+
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      {/* Triggeren åbner/lukker menuen */}
+      {/* Vi bruger `asChild` for selv at kunne styre hvilket element der bruges som trigger.
+          Uden `asChild` ville DropdownMenuTrigger selv lave en <button>, og så får vi ulovlig HTML
+          med en <button> inde i en anden <button> – det giver en fejl i browseren og i Next.js (hydration error) */}
+      <DropdownMenuTrigger asChild>
+        <button
+          className="flex items-center gap-2 text-[32px] font-light text-black bg-transparent border border-[#726572] focus:outline-none focus:ring-1 focus:ring-[#726572] focus:ring-offset-1"
+        >
+          Lokation
+          {/* Pil der roterer når dropdown er åben */}
+          <IoIosArrowDown
+            className={`text-[32px] transition-transform duration-300 ${open ? "rotate-180" : "rotate-0"
+              }`}
+          />
+        </button>
+      </DropdownMenuTrigger>
+
+      {/* Indholdet i dropdown-menuen */}
+      <DropdownMenuContent className="bg-white">
+        <DropdownMenuLabel className="text-black">Lokationer</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem>Århus</DropdownMenuItem>
+        <DropdownMenuItem>København</DropdownMenuItem>
+        <DropdownMenuItem>Køge</DropdownMenuItem>
+        <DropdownMenuItem>Odense</DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu ></div>;
-};
-
-export default DropDownLocation;
+    </DropdownMenu>
+  );
+}
