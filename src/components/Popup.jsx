02401a14fa9f17cbb4429a8826fbd6fc39bdd88f
Mariawillow <mani0013@stud.kea.dk>
2025-05-12 09:51:16 +0200

    popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,19 +1,9 @@
-// import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-// const Popup = () => {
-//   return <div></div>;
-// };
-
-// export default function page() {
-//   return (
-//     <main className="p-6">
-//       <SignedIn>
-//         <p>Velkommen tilbage!</p>
-//         <UserButton />
-//       </SignedIn>
-//       <SignedOut>
-//         <p>Du er ikke logget ind.</p>
-//       </SignedOut>
-//     </main>
-//   );
-// }
+const Popup = () => {
+  return (  
+    <div></div>
+  );
+}
+ 
+export default Popup;
