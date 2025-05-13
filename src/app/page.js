67f085a8b805963d64700fec414274e8dd5f3390
Mariawillow <mani0013@stud.kea.dk>
2025-05-13 18:05:30 +0200

    kan ikke få img som bg, men har fikset det andet

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,28 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import ButtonPrimary from "@/components/ButtonPrimary";
-import ButtonSecondary from "@/components/ButtonSecondary";
-import Kurv from "@/components/Basket";
-import Popup from "@/components/Popup";
-import { SignIn } from "@clerk/nextjs";
+import StatuePic from "@/images/statuePic.svg";
+
+
 
 export default function Home() {
   return (
-    <div className="col-start-2">
-      <Header></Header>
-      <h1>Dav</h1>
-      <h2>Dav</h2>
-      <h3 className="mt-space-m">Dav</h3>
-      <h4>Dav</h4>
-      <h5>Dav</h5>
-      <p>Dav</p>
+    
+    
+<div
+      className="col-start-2 h-[800px] bg-[url(/images/statuePic.jpg)]  bg-cover bg-no-repeat bg-center relative"
+    >
+    <Header />
 
-      {/* <ButtonPrimary variant="primary">Primær</ButtonPrimary>
-      <ButtonPrimary variant="secondary">Sekundær</ButtonPrimary>
-      <ButtonPrimary variant="danger">Slet</ButtonPrimary>
-      <ButtonPrimary variant="ukendt">Fallback</ButtonPrimary> */}
+      <h1 className="text-[#C4FF00] leading-relaxed">
+        Oplev Storartet Kunst <br /> På Feler Måder
+      </h1>
 
-      <ButtonPrimary size="large">Button</ButtonPrimary>
-      <ButtonPrimary size="small">Button</ButtonPrimary>
-
-      <ButtonSecondary></ButtonSecondary>
-
-      <Popup></Popup>
+      <ButtonPrimary
+        size="small"
+        className="absolute bottom-4 right-4">
+        Events
+      </ButtonPrimary>
     </div>
   );
 }
