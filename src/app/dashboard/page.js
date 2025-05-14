267f840efe3455b65a9fbddd31373a647b4a94ab
Mariawillow <mani0013@stud.kea.dk>
2025-05-14 17:55:50 +0200

    header på dash

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,12 @@
 import DashCard from "@/components/(dashboard)/DashCard";
+import Header from "@/components/Header";
+
 
 const Dashboard = () => {
     return ( 
         <article>
+                  <Header variant="black"></Header>
+
             <h1>Dine oprettede events</h1>
             <DashCard />
         </article>
