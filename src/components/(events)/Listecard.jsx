be66efa8a84745daab7f2bdd3672c5565d8bb5fd
Anarikke <anla0008@stud.kea.dk>
2025-05-16 13:53:28 +0200

    Test på listeCard

--- src/components/(events)/Listecard.jsx
+++ src/components/(events)/Listecard.jsx
@@ -7,7 +7,7 @@ const ListeCard = ({ event }) => {
     <Link href={`/event/${event.id}`} className="relative aspect-[3/2] group w-full cursor-pointer">
       <Image src={StatuePic} alt="statuebillede" width={300} height={200} className="w-full h-full object-cover transition duration-300 group-hover:bg-[#DAD6DD] group-hover:opacity-40 group-hover:border-[3px] group-hover:border-[#C4FF00]" />
       <div className="absolute inset-0 flex flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <h4 className="text-black font-semibold">{event.title}</h4>
+        <h4 className="text-pink font-semibold">{event.title}</h4>
         <h3 className="text-black font-light">459 DKK</h3>
         <p className="text-black font-semibold">{event.date}</p>
       </div>
