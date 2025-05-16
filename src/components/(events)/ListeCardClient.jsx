457dcde184e606de707630610ba50ad01c2276ff
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-16 14:23:04 +0200

    Ny liste card ny

--- src/components/(events)/ListeCardClient.jsx
+++ src/components/(events)/ListeCardClient.jsx
@@ -1,11 +1,11 @@
 "use client";
-import ListeCard from "./ListeCard";
+import ListeCardNy from "./ListeCardNy";
 
 const ListeCardClient = ({ events }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
       {events.map((event) => (
-        <ListeCard key={event.id} event={event} />
+        <ListeCardNy key={event.id} event={event} />
       ))}
     </div>
   );
