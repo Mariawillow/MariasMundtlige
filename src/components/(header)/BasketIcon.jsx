14acee3c90a089759f03a52d5172774ad09f3553
Mariawillow <mani0013@stud.kea.dk>
2025-05-21 21:05:35 +0200

    retttelse på nr

--- src/components/(header)/BasketIcon.jsx
+++ src/components/(header)/BasketIcon.jsx
@@ -8,7 +8,8 @@ import useCartStore from "@/app/store/cartStore"
 
 const BasketIcon = ({ variant = "lime" }) => {
     const { items } = useCartStore();
-    return ( <div>
+    return ( 
+        <div className="relative inline-block">
 <Basket variant={variant} />
 {items.length > 0 && 
 <span className="absolute bottom-0 right-0 bg-[#C4FF00] text-white rounded-full text-xs px-2 py-1">
