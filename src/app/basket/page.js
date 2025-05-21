d0e81feba64b137cfd4e1bbc292ba9180eae349a
Mariawillow <mani0013@stud.kea.dk>
2025-05-21 19:47:57 +0200

    update

--- src/app/basket/page.js
+++ src/app/basket/page.js
@@ -35,7 +35,7 @@ const Basket = () => {
             <h2 className="font-light">Billetter</h2>
           </div>
 
-          <div>{items && items.length > 0 ? items.map((item) => <CartCard key={item.id} item={item} />) : <p>Du har ikke valgt nogle billetter.. </p>}</div>
+          <div>{items && items.length > 0 ? items.map((item) => <useCartStore key={item.id} item={item} />) : <p>Du har ikke valgt nogle billetter.. </p>}</div>
 
           {/* Billetter */}
           <Price />
