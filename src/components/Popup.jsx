1f48de6a44d9f8a8f3afcbfa53e9c5cfe25613b7
Mariawillow <mani0013@stud.kea.dk>
2025-05-22 14:56:30 +0200

    design

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,13 @@
 // components/Popup.js
 const Popup = ({ onClose }) => {
   return (
-    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
-      <div className="bg-white p-10 text-center shadow-xl max-w-md w-full border-10 border-[#C4FF00]">
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-xs flex items-center justify-center z-50">
+      <div className="bg-[#bab0bc] p-40 ml-50 text-center w-full border-t-10 border-l-10 border-b-10 
+  border-t-[#C4FF00] border-l-[#C4FF00] border-b-[#C4FF00]
+  sm:p-40 sm:ml-50 
+  sm:border-t-10 sm:border-l-10 sm:border-b-10 
+  sm:border-t-[#C4FF00] sm:border-l-[#C4FF00] sm:border-b-[#C4FF00]
+  ">
         <h2 className="text-xl font-bold mb-4">Tak for din booking!</h2>
         <p className="mb-6">Vi glæder os til at se dig</p>
         <button
