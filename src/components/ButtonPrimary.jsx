dd1c2ed45ed64a910d3f78552341d52e8123a0bd
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-23 10:20:45 +0200

    knap og forside done

--- src/components/ButtonPrimary.jsx
+++ src/components/ButtonPrimary.jsx
@@ -1,23 +1,14 @@
-import { HiArrowLongRight } from "react-icons/hi2";
 import Link from "next/link";
+import Image from "next/image";
+import arrowLong from "@/images/arrowLong.svg";
 
-const ButtonPrimary = ({ href, size = "large", children, className = "" }) => {
-  // Sætter en betingelse for tekststørrelsen og ikonet afhængig af size prop'en
-  const isLarge = size === "large";
-
-  const textSize = isLarge ? "text-4xl font-bold" : "text-2xl"; // Størrelse på teksten
-  const iconSize = isLarge ? 75 : 24; // Størrelse på pilen
-
-  const baseClasses = `group inline-block text-[#C4FF00] cursor-pointer ${className}`;
-
-  return (
-    <Link href={href} className={baseClasses}>
-      <span className="inline-flex flex-col">
-        <span className={`${textSize} px-8`}>{children}</span>
-        <HiArrowLongRight size={iconSize} className="-mt-6 self-end transition-transform group-hover:translate-x-1" />
-      </span>
-    </Link>
-  );
-};
+const ButtonPrimary = ({ href, children }) => (
+  <Link href={href} className="group inline-block text-[#C4FF00] cursor-pointer">
+    <span className="inline-flex flex-col">
+      <span className="text-4xl font-bold px-8">{children}</span>
+      <Image src={arrowLong} alt="pil" className="self-end transition-transform group-hover:translate-x-1"></Image>
+    </span>
+  </Link>
+);
 
 export default ButtonPrimary;
