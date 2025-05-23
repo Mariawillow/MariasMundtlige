39278ceb73a022a29ee4c11dcef5512dbbdf2405
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-23 15:29:24 +0200

    fjernet async-mappe – tilføjet ved en fejl

--- src/components/(event)/EventArt.jsx
+++ src/components/(event)/EventArt.jsx
@@ -17,22 +17,15 @@ const EventArt = ({ art }) => {
         height={200}
         className="w-full h-full object-cover transition duration-300 group-hover:bg-[#DAD6DD] group-hover:opacity-40 group-hover:border-[3px] group-hover:border-[#C4FF00]"
       />
-        <div className="hidden sm:flex absolute inset-0 flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition duration-300">
+      <div className="hidden sm:flex absolute inset-0 flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition duration-300">
         <h4 className="text-black font-semibold">{title}</h4>
       </div>
 
       <div className="sm:hidden mb-6  mt-3 text-center">
         <h4 className="text-black font-semibold">{title}</h4>
-        </div>
+      </div>
     </Link>
   );
 };
 
 export default EventArt;
-
-//  {/* Overlay text (hidden on small screens) */}
-//  <div className="hidden sm:flex absolute inset-0 flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition duration-300">
-//  <h4 className="text-black font-semibold">{event.title}</h4>
-//  <h3 className="text-black font-light">459 DKK</h3>
-//  <p className="text-black font-semibold">{event.date}</p>
-// </div>
