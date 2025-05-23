8aa9586e02f81e95dddc1ff359fca52b56582843
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-23 14:26:36 +0200

    styling af opret event side done

--- src/components/(newEvent)/ArtWorkGrid.jsx
+++ src/components/(newEvent)/ArtWorkGrid.jsx
@@ -1,15 +1,37 @@
 import ArtworkCard from "./ArtWorkCard";
+import { useState } from "react";
 
 export default function ArtworkGrid({ artworks, selectedArtworks, toggleArtwork }) {
   if (!artworks.length) {
     return <p className="text-center text-gray-500">Ingen værker fundet i den valgte periode.</p>;
   }
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMS_PER_PAGE = 12; //Antal værker pr. side
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const currentArtworks = artworks.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(artworks.length / ITEMS_PER_PAGE); //Regner antal sider i alt
+
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      {artworks.map((artwork) => (
-        <ArtworkCard key={artwork.id} artwork={artwork} selected={selectedArtworks} onClick={toggleArtwork} />
-      ))}
+    <div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-space-s">
+        {currentArtworks.map((artwork) => (
+          <ArtworkCard key={artwork.id} artwork={artwork} selected={selectedArtworks} onClick={toggleArtwork} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        <button onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))} disabled={currentPage === 1} className={`p-4 disabled:opacity-50 ${currentPage !== 1 ? "cursor-pointer" : "cursor-default"}`}>
+          {`<`}
+        </button>
+
+        <span className="flex items-center">
+          Side {currentPage} af {totalPages}
+        </span>
+
+        <button onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))} disabled={currentPage === totalPages} className={`p-4 disabled:opacity-50 ${currentPage !== totalPages ? "cursor-pointer" : "cursor-default"}`}>
+          {`>`}
+        </button>
+      </div>
     </div>
   );
 }
