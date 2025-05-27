be1ec50c354adc7077ce794ae8feef80bf978803
Mariawillow <mani0013@stud.kea.dk>
2025-05-27 09:48:52 +0200

    Udsolgt badge

--- src/components/(events)/ListeCardNy.jsx
+++ src/components/(events)/ListeCardNy.jsx
@@ -7,18 +7,29 @@ const ListeCardNy = ({ event }) => {
   const remainingTickets = event.totalTickets - event.bookedTickets;
   const imageUrl = event.thumbnailImage || StatuePic;
 
+
   return (
     <Link href={`/event/${event.id}`} className="group w-full cursor-pointer">
       <div className="relative aspect-[3/2]">
         <Image src={imageUrl} alt="statuebillede" width={300} height={200} className="w-full h-full object-cover transition duration-300 group-hover:bg-[#DAD6DD] group-hover:opacity-40 group-hover:border-[3px] group-hover:border-[#C4FF00]" />
 
-        {/* BADGE: Vis kun hvis < 10 billetter */}
-        {remainingTickets < 10 && <div className="absolute top-2 right-2 bg-gray-50 text-black text-xs font-bold px-2 py-1 z-10">Få billetter tilbage</div>}
+            {/* BADGE: Viser kun én af dem – Udsolgt har højeste prioritet */}
+            {/* Fortæller at enten 0 eller under */}
+      {remainingTickets <= 0 ? (
+        <div className="absolute top-2 right-2 bg-rose-300 text-black text-xs font-bold px-2 py-1 z-10">
+          Udsolgt
+        </div>
+      ) : remainingTickets < 10 ? (
+        <div className="absolute top-2 right-2 bg-gray-50 text-black text-xs font-bold px-2 py-1 z-10">
+          Få billetter tilbage
+        </div>
+        // Og "null" ellers vi ingenting 
+      ) : null}
 
         {/* Overlay text (hidden on small screens) */}
         <div className="hidden sm:flex absolute inset-0 flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition duration-300">
           <h4 className="text-black font-semibold">{event.title}</h4>
-          <h3 className="text-black font-light">459 DKK</h3>
+          <h3 className="text-black font-light">170 DKK</h3>
           <p className="text-black font-semibold">{event.date}</p>
         </div>
       </div>
@@ -26,7 +37,7 @@ const ListeCardNy = ({ event }) => {
       {/* Text below image (visible only on small screens) */}
       <div className="sm:hidden mt-2 text-center">
         <h4 className="text-black font-semibold">{event.title}</h4>
-        <h3 className="text-black font-light">459 DKK</h3>
+        <h3 className="text-black font-light">170 DKK</h3>
         <p className="text-black font-semibold">{event.date}</p>
       </div>
     </Link>
