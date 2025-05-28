3b006fb6831f62bd44cd0d5c33b96637b15cb56e
Mariawillow <mani0013@stud.kea.dk>
2025-05-28 13:37:12 +0200

    kavet det sammen som anarikke

--- src/app/event/[id]/page.js
+++ src/app/event/[id]/page.js
@@ -9,7 +9,7 @@ const EventSingleView = async ({ params }) => {
   //Denne første del burde vi overveje at flytte til API-side (localhost.js) – meeeen vi kan ikke lige finde ud af hvordan
   const { id } = await params;
 
-  const res = await fetch(`https://smk-4l23.onrender.com/events/${id}`); //https://smk-4l23.onrender.com/events/${id}
+  const res = await fetch(`http://localhost:8080/events/${id}`); //https://smk-4l23.onrender.com/events/${id}
   const eventData = await res.json();
 
   // Hent kunstværkerne fra SMK API (eller specifikke værker baseret på IDs)
