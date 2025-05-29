6860e06fa978bc113044f375d777428a9d041812
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-29 12:28:11 +0200

    rettelser fra merge konflikt

--- src/components/(newEvent)/SearchBar.jsx
+++ src/components/(newEvent)/SearchBar.jsx
@@ -4,29 +4,15 @@ import { IoIosSearch } from "react-icons/io";
 export const SearchBar = ({ onSearch }) => {
   const [input, setInput] = useState("");
 
-  const fetchData = async (malerier) => {
-    const baseFilters = ["has_image:true", "on_display:false", "public_domain:true"];
-    const filterString = baseFilters.map((f) => `[${f}]`).join(",");
-    const url = `https://api.smk.dk/api/v1/art/search/?keys=${malerier}&filters=${filterString}&offset=0&rows=2000`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setResults(data.items || []);
-    } catch (error) {
-      console.error("Fejl ved hentning af data:", error);
-    }
-  };
-
   const handleChange = (value) => {
     setInput(value);
-    fetchData(value);
+    onSearch(value); // Giver søgeteksten videre op
   };
 
   return (
     <div className="input-wrapper">
-      <IoIosSearch id="serach-icon" className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none" />
-      <input placeholder="Søg efter events her.." value={input} onChange={(e) => handleChange(e.target.value)} className="w-full border rounded px-3 py-2 pr-10" />
+      <IoIosSearch id="search-icon" className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none" />
+      <input placeholder="Søg efter kunstnere her..." value={input} onChange={(e) => handleChange(e.target.value)} className="w-full border rounded px-3 py-2 pr-10" />
     </div>
   );
 };
--- src/components/(newEvent)/SearchResultsList.jsx
+++ src/components/(newEvent)/SearchResultsList.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import {SearchResults} from "./SearchResults"
+import { SearchResults } from "./SearchResults";
 
-const SearchResultsList = ({results}) => {
-    return ( <div className="results-list w-full bg-white flex flex-col mt-4 max-h-[300px] overflow-y-scroll rounded-[10px] ">
-        {
-            results.map((result, id) => {
-                return <SearchResults result={result} key={id}  />
-            })
-        }
+const SearchResultsList = ({ results }) => {
+  return (
+    <div className="results-list w-full bg-white flex flex-col mt-4 max-h-[300px] overflow-y-scroll rounded-[10px] ">
+      {results.map((result, id) => {
+        return <SearchResults result={result} key={id} />;
+      })}
+    </div>
+  );
+};
 
-    </div> );
-}
- 
-export default  SearchResultsList;
+export default SearchResultsList;
