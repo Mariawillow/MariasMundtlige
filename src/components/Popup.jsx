8e57ef292a9f1a6293efc48f3ea41422a4c57217
Anarikke <anla0008@stud.kea.dk>
2025-05-30 22:18:52 +0200

    use client

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,18 +1,4 @@
-// const Popup = ({ onClose, message }) => {
-//   return (
-//     <div className="fixed inset-0 bg-black/30 backdrop-blur-xs flex items-center justify-center z-50">
-//       <div className="bg-[#bab0bc] p-10 text-center border-7 border-[#C4FF00] max-w-md mx-4 rounded-lg">
-//         <p className="text-lg mb-6">{message}</p>
-//         <button onClick={onClose} className="bg-[#C4FF00] px-4 py-2 rounded text-black font-semibold hover:cursor-pointer">
-//           Luk
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Popup;
-
+"use client"
 
 const Popup = ({ onClose, onConfirm, message, confirmText = "OK", cancelText = "Luk", showConfirm = false }) => {
   return (
