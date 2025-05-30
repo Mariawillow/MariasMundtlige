29794a8212b488d0a6136d51376b6767c06054f7
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-05-30 13:57:30 +0200

    oprydning i header variant + komponenter

--- src/components/(header)/Basket.jsx
+++
@@ -1,9 +0,0 @@
-import { HiOutlineShoppingBag } from "react-icons/hi";
-
-const Basket = ({ variant = "lime" }) => {
-  const isLime = variant === "lime";
-
-  return <HiOutlineShoppingBag size={40} className={`cursor-pointer ${isLime ? "text-[#C4FF00]" : "text-black"}`} />;
-};
-
-export default Basket;
--- src/components/(header)/BasketIcon.jsx
+++ src/components/(header)/BasketIcon.jsx
@@ -1,15 +1,17 @@
 "use client";
 
-import Basket from "./Basket";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 import useCartStore from "@/app/store/cartStore";
 
-const BasketIcon = ({ variant = "lime" }) => {
+const BasketIcon = ({ variant = "black" }) => {
   const { items } = useCartStore();
+  const isLime = variant === "lime";
+  const itemCount = items.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div className="relative inline-block">
-      <Basket variant={variant} />
-      {items.length > 0 && <span className="absolute bottom-0 right-0 bg-[#C4FF00] text-black rounded-full text-xs px-2 py-1">
-        {items.reduce((total, item) => total + item.quantity, 0)}</span>}
+      <HiOutlineShoppingBag size={40} className={`cursor-pointer ${isLime ? "text-[#C4FF00]" : "text-black"}`} />
+      {itemCount > 0 && <span className={`absolute bottom-0 right-0 rounded-full text-xs font-bold px-2 py-0.5 bg-[#C4FF00]`}>{itemCount}</span>}
     </div>
   );
 };
