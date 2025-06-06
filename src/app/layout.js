3f90e4fd82b4b00124d7d65c08f7902cd800b024
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-06-06 12:52:39 +0200

    endnu en preconnect

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
       <html lang="da">
         <head>
           <link rel="preconnect" href="https://smk-4l23.onrender.com" />
+          <link rel="preconnect" href="https://api.smk.dk" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
