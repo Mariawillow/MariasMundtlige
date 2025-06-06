eb8855a40dd12010cf391973fb4f943bee4aebdf
Anarikke <anla0008@stud.kea.dk>
2025-06-06 15:23:37 +0200

    label justering på step 1

--- src/components/(newEvent)/DateSelector.jsx
+++ src/components/(newEvent)/DateSelector.jsx
@@ -5,8 +5,9 @@ import DatePicker from "@/components/(newEvent)/DatePicker";
 export default function DateSelector({ date, setDate, location }) {
   return (
     <div className="flex flex-col">
-      <label className="text-sm font-medium">Dato *</label>
-      <DatePicker date={date} setDate={setDate} location={location} />
+      <label className="text-sm font-medium">Dato *
+        <DatePicker date={date} setDate={setDate} location={location} />
+      </label>
     </div>
   );
 }
--- src/components/(newEvent)/PeriodSelector.jsx
+++ src/components/(newEvent)/PeriodSelector.jsx
@@ -5,25 +5,26 @@ import { periods } from "@/api/periods";
 export default function PeriodSelector({ period, setPeriod }) {
   return (
     <div className="flex flex-col">
-      <label className="text-sm font-medium">Kunstperiode *</label>
-      <select
-        value={period?.id || ""}
-        onChange={(e) => {
-          const selectedId = e.target.value;
-          const selected = periods.find((p) => p.id === selectedId);
-          setPeriod(selected);
-        }}
-        className="border px-3 py-2"
-      >
-        <option value="" disabled>
-          Vælg periode
-        </option>
-        {periods.map((p) => (
-          <option key={p.id} value={p.id}>
-            {p.name}
+      <label className="text-sm font-medium">Kunstperiode *
+        <select
+          value={period?.id || ""}
+          onChange={(e) => {
+            const selectedId = e.target.value;
+            const selected = periods.find((p) => p.id === selectedId);
+            setPeriod(selected);
+          }}
+          className="border px-3 py-2"
+        >
+          <option value="" disabled>
+            Vælg periode
           </option>
-        ))}
-      </select>
+          {periods.map((p) => (
+            <option key={p.id} value={p.id}>
+              {p.name}
+            </option>
+          ))}
+        </select>
+      </label>
     </div>
   );
 }
